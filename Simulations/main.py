6399512b8bb0d159b172a696b87b2ecbe05d86e2
annikahe <alhennes97@gmail.com>
2021-10-28 03:55:20 +0200

    Plot qualities of different instances of FtP^ONL_lambda

--- Simulations/main.py
+++ Simulations/main.py
@@ -1,56 +1,67 @@
-import copy
-
 import algorithms as alg
-import offline as off
-import predictions as pred
-import testing as test
-from history import History
-import pandas as pd
 import matplotlib.pyplot as plt
 import plots
 import simulate
+import pandas as pd
+import numpy as np
 
-# exchange_rates = [1, 2, 3, 1, 4, 1, 6, 1, 1, 1, 2, 1, 6, 4, 2, 3, 1, 8]
-exchange_rates = [3, 2, 1, 3, 2, 1, 1, 2, 3, 3, 3, 4, 5, 1]
-phi = max(exchange_rates)
 
-# alg1 = alg.AlgorithmRandom()
-# alg2 = alg.AlgorithmRandomTrade()
-#
-# pred = pred.opt_off_distorted(exchange_rates)
-# alg_pred = alg.FtP(pred)
-# print(test.alg_return(copy.deepcopy(alg_pred), exchange_rates))
-#
-# h = History(alg_pred, exchange_rates)
-# h.run_full()
-# print(h.get_results_str())
-#
-# h1 = History(alg1, exchange_rates)
-# h1.run_full()
-# print(h1.get_results_str())
-#
-# h2 = History(alg2, exchange_rates)
-# h2.run_full()
-# print(h2.get_results_str())
+k = np.inf
+phi = 100
+length = 100  # length of the input sequence
+normalized = False
+num_rep = 1000
+alpha = 1  # 0.7
+beta = 0  # 0.2
+# alpha = [0.7, 0.1, 0.1]
+# beta=[0.2, 0.45, 0.1]
+all_combinations = False
+data_model = "DataIterative"
 
-# eta = []
-# cr = []
-# for i in range(10000):
-#     p = pred.opt_off_distorted(exchange_rates)
-#     alg_pred = alg.FtP(p)
+# plots.plot_ftp(k, phi, length, data_model, num_rep, normalized, alpha, beta, all_combinations)
+
+alg0_name = "ftp"
+alg1_name = "opt"
+# l = 0.5
+lambda_list = [0, 0.2, 0.4, 0.6, 0.8, 1]  # list(np.linspace(0, 1, 6))
+comb_type = "start"
+
+# ax = None
 #
-#     h = History(alg_pred, exchange_rates)
-#     h.run_full()
-#     eta.append(h.get_total_error())
-#     cr.append(h.get_comp_ratio())
+# for l in np.linspace(0, 1, 6):
+#     l = round(l, 1)
+#     ax = plots.plot_combined(ax, k, phi, length, alg0_name, alg1_name, l, comb_type, data_model, num_rep, normalized, alpha, beta, all_combinations)
+# ax = plots.plot_combined(ax, k, phi, length, alg0_name, alg1_name, 0.7, comb_type, data_model, num_rep, normalized, alpha, beta, all_combinations)
 
-length = 1000
-eta, cr = simulate.simulate(phi, length)
+df = simulate.simulate_combined_alg_mult(k, phi, length, alg0_name, alg1_name, lambda_list, comb_type,
+                      data_model, num_rep, normalized, alpha, beta, all_combinations)
 
-plots.error_against_comp_ratio(eta, cr)
-x = range(length)
-y = [1/(phi**(err/2)) for err in x]
-plt.plot(x, y, label="Lower bound")
+print(df)
+
+ax = df.plot()
+
+xlim = ax.get_xlim()
+x = range(int(xlim[1]) + 1)
+y = [(phi ** (err / 2)) for err in x]
+plt.plot(x, y, label="Competitive Ratio")
+plt.legend()
+
+plt.yscale("log")
+ax.set_ylabel("$return(OFF) / return(A)$")
+plt.setp(ax.lines, linewidth=1)
+
+if normalized:
+    plt.savefig(f"Plots/Combined_{alg0_name}_{alg1_name}_k{k}_{data_model}_alpha{alpha}_beta{beta}_length{length}_repetitions{num_rep}_normalized_error.svg")
+else:
+    plt.savefig(f"../Plots/Combined_{alg0_name}_{alg1_name}_k{k}_{data_model}_alpha{alpha}_beta{beta}_length{length}_repetitions{num_rep}.svg")
+
+# d = {"$\eta / return(OFF)$": [], f"return(OFF) / return({alg_name})": []}
+# df = pd.DataFrame([])
+#
+# eta, cr = simulate.simulate_combined_alg(k, phi, length, alg0_name, alg1_name, l, comb_type, data_model,
+#                                          num_rep, normalized, alpha, beta, all_combinations)
+
+
 plt.show()
 
 
