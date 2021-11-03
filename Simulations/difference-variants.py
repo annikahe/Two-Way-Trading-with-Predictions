013ca11a54ad725c57ecc8d73cdfd207528de293
annikahe <alhennes97@gmail.com>
2021-11-03 14:25:47 +0100

    Plot comparison of the two FtP_lambda versions

---
+++ Simulations/difference-variants.py
@@ -0,0 +1,64 @@
+import algorithms as alg
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import plots
+import simulate
+import pandas as pd
+import numpy as np
+from decimal import Decimal
+from matplotlib.ticker import MaxNLocator
+
+
+k = np.inf
+phi = 10
+length = 10  # length of the input sequence
+normalized = False
+num_rep = 1000
+alpha = 0.1
+beta = 0.1
+# alpha = [0.7, 0.1, 0.1]
+# beta=[0.2, 0.45, 0.1]
+all_combinations = False
+data_model = "DataUniform"
+alg0_name = "ftp"
+alg1_name = "opt"
+lambda_list = [0, 0.25, 0.5, 0.75, 0.99]
+
+# df_start = simulate.simulate_combined_alg_mult(k, phi, length, alg0_name, alg1_name, lambda_list, "start",
+#                                          data_model, num_rep, normalized, alpha, beta, all_combinations)
+#
+# df_step = simulate.simulate_combined_alg_mult(k, phi, length, alg0_name, alg1_name, lambda_list, "step",
+#                                          data_model, num_rep, normalized, alpha, beta, all_combinations)
+#
+# df = df_step.subtract(df_start, fill_value=0)  # TODO: difference of dfs
+# print(df)
+
+eta, cr = simulate.compare_ftpl_types(k, phi, length, alg0_name, alg1_name, lambda_list, data_model, num_rep, alpha, beta, all_combinations)
+
+column_names = [f"$\lambda = {l}$" for l in lambda_list]
+df = pd.DataFrame(data=np.array(cr).transpose(), index=eta, columns=column_names)
+df = df.groupby(df.index).mean()
+
+cmap = cm.get_cmap('plasma_r')
+ax = df.plot(grid=True, colormap=cmap, use_index=True)
+
+xlim = ax.get_xlim()
+if xlim[1] > 16:
+    ax.set_xlim(0, 16)
+    xlim = ax.get_xlim()
+plt.legend()
+
+plt.yscale("symlog")
+ax.set_xlabel("$\eta$")
+ax.set_ylabel("$return(OFF) / return(FtP_{\lambda}^{ONL})$")
+plt.setp(ax.lines, linewidth=2)
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+# ax.set_ylim(1, 10**3)
+
+plt.savefig(f"../Plots/Difference-Combined-{alg0_name}-{alg1_name}-Step-min-Start-k_{k}-{data_model}-alpha_{alpha}-beta_{beta}-length_{length}-repetitions_{num_rep}.pdf")
+
+
+plt.show()
+
+
+
--- Simulations/simulate.py
+++ Simulations/simulate.py
@@ -120,6 +120,60 @@ def simulate_combined_alg(k, phi, length, alg0_name, alg1_name, lambda_list, com
     return eta, cr
 
 
+def compare_ftpl_types(k, phi, length, alg0_name, alg1_name, lambda_list, data_model="DataIterative", num_rep=1000, alpha=0.7, beta=0.2, all_combinations=False):
+
+    alpha_beta_list = generate_alpha_beta_list(alpha, beta, all_combinations)
+
+    eta = []
+    cr = [[] for l in lambda_list]
+
+    for i in tqdm(range(num_rep)):
+        for alpha, beta in alpha_beta_list:
+            for sigma in [0, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100]:
+                if data_model == "DataIterative":
+                    exchange_rates = inst.exchange_rates_normal_from_last(phi, length, sigma)
+                else:  # elif inst_type == "DataUniform":
+                    exchange_rates = inst.exchange_rates_uniform(phi, length)
+
+                p = pred.opt_off_distorted(exchange_rates, alpha, beta)
+                ftp = alg.FtP(k, p)
+                h_ftp = History(ftp, p, exchange_rates)
+                h_ftp.run_full()
+                eta.append(h_ftp.get_total_error())
+
+                for index, l in enumerate(lambda_list):
+                    if alg0_name.lower() == "ftp":
+                        alg0_start = alg.FtP(k, p)
+                        alg0_step = alg.FtP(k, p)
+                    elif alg0_name.lower() == "opt":
+                        alg0_start = alg.OptDeterministic(k, phi)
+                        alg0_step = alg.OptDeterministic(k, phi)
+                    else:
+                        return 1
+
+                    if alg1_name.lower() == "ftp":
+                        alg1_start = alg.FtP(k, p)
+                        alg1_step = alg.FtP(k, p)
+                    elif alg1_name.lower() == "opt":
+                        alg1_start = alg.OptDeterministic(k, phi)
+                        alg1_step = alg.OptDeterministic(k, phi)
+                    else:
+                        return 1
+
+                    alg_combined_start = alg.CombineRandStart(k, alg0_start, alg1_start, l)
+                    alg_combined_step = alg.CombineRandStep(k, alg0_step, alg1_step, l)
+
+                    h_start = History(alg_combined_start, p, exchange_rates)
+                    h_start.run_full()
+                    h_step = History(alg_combined_step, p, exchange_rates)
+                    h_step.run_full()
+                    cr[index].append(h_step.get_comp_ratio() - h_start.get_comp_ratio())
+
+    print(cr)
+
+    return eta, cr
+
+
 def simulate_combined_alg_mult(k, phi, length, alg0_name, alg1_name, lambda_list, comb_type="start",
                       data_model="DataIterative", num_rep=1000, normalized=False, alpha=0.7, beta=0.2,
                       all_combinations=False):
